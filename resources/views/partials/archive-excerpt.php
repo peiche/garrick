e3650a0cfcfb4b3e44065361ae7a288cad497f2d
Paul <peiche@gmail.com>
2019-11-19 17:35:02 -0600

    Added read more link with setting

--- resources/views/partials/archive-excerpt.php
+++ resources/views/partials/archive-excerpt.php
@@ -1,5 +1,12 @@
 <?php if ( ! get_theme_mod( 'archive_hide_excerpt', false ) ) : ?>
 	<div class="entry__summary margin-y-sm">
 		<?php the_excerpt() ?>
+
+		<?php if ( ! get_theme_mod( 'archive_hide_readmore', false ) ) : ?>
+			<a href="<?php echo esc_url( get_permalink() ) ?>" class="btn btn--primary">
+				<?php echo esc_html__( 'Read More', 'garrick' ) ?>
+				<?php echo \Garrick\get_svg( [ 'icon' => 'arrow-right', 'class' => 'margin-left-xxs' ] ) ?>
+			</a>
+		<?php endif ?>
 	</div>
 <?php endif ?>
