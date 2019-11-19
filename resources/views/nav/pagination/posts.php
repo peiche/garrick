5f493b3952f1c210ce53f66c9f6067f7913bc586
Paul <peiche@gmail.com>
2019-11-19 17:34:48 -0600

    Removed non-breaking spaces

--- resources/views/nav/pagination/posts.php
+++ resources/views/nav/pagination/posts.php
@@ -1,5 +1,5 @@
 <?php Hybrid\Pagination\display( 'posts', [
-	'prev_text'  => \Garrick\get_svg( [ 'icon' => 'arrow-left' ] ) . '&nbsp;' . __( 'Previous', 'garrick' ),
-	'next_text'  => __( 'Next', 'garrick' ) . '&nbsp;' . \Garrick\get_svg( [ 'icon' => 'arrow-right' ] ),
+	'prev_text'  => \Garrick\get_svg( [ 'icon' => 'arrow-left', 'class' => 'margin-right-xxs' ] ) . __( 'Previous', 'garrick' ),
+	'next_text'  => __( 'Next', 'garrick' ) . \Garrick\get_svg( [ 'icon' => 'arrow-right', 'class' => 'margin-left-xxs' ] ),
 	'title_text' => __( 'Posts Navigation', 'garrick' )
 ] );
