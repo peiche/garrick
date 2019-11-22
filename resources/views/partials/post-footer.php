d0df86f19e056a0c0f96cc87dafa8959b7b9261a
Paul <peiche@gmail.com>
2019-11-22 17:11:28 -0600

    Fixed icon alignment and updated taxonomy link display.

--- resources/views/partials/post-footer.php
+++ resources/views/partials/post-footer.php
@@ -1,34 +1,30 @@
 <footer class="entry__footer margin-top-md">
 
 	<?php if ( has_category() ) : ?>
-		<div class="flex flex-gap-xxs margin-bottom-xxxs items-center">
+		<div class="flex flex-gap-xxs margin-bottom-xxxs">
 			<div class="flex-shrink-0 margin-top-xxxs">
 				<?php echo \Garrick\get_svg( [ 'icon' => 'category', 'title' => __( 'Categories', 'garrick' ) ] ) ?>
 			</div>
 			<div>
-				<?php Hybrid\Post\display_terms( [
-					'taxonomy' => 'category',
-					'class' => 'entry__terms entry__terms--category flex flex-wrap flex-gap-xxs',
-					'sep' => ', ',
-				] ) ?>
+				<?php Hybrid\View\display( 'partials', 'post-terms', [ 'taxonomy' => 'category' ] ) ?>
 			</div>
 		</div>
 	<?php endif ?>
 
 	<?php if ( has_tag() ) : ?>
-		<div class="flex flex-gap-xxs margin-bottom-xxxs items-center">
+		<div class="flex flex-gap-xxs margin-bottom-xxxs">
 			<div class="flex-shrink-0 margin-top-xxxs">
 				<?php echo \Garrick\get_svg( [ 'icon' => 'tag', 'title' => __( 'Tags', 'garrick' ) ] ) ?>
 			</div>
 			<div>
-				<?php Hybrid\View\display( 'partials', 'post-tags' ) ?>
+				<?php Hybrid\View\display( 'partials', 'post-terms', [ 'taxonomy' => 'post_tag' ] ) ?>
 			</div>
 		</div>
 	<?php endif ?>
 
 	<?php edit_post_link(
 		esc_html__( 'Edit', 'garrick' ),
-		'<div class="flex flex-gap-xxs margin-bottom-xxxs items-center"><div class="flex-shrink-0">' . \Garrick\get_svg( [ 'icon' => 'edit' ] ) . '</div><div>',
+		'<div class="flex flex-gap-xxs margin-bottom-xxxs"><div class="flex-shrink-0">' . \Garrick\get_svg( [ 'icon' => 'edit' ] ) . '</div><div>',
 		'</div></div>'
 	) ?>
 
