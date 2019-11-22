d6e52b0c5d5eb7231958c564c43a83036b6e783a
Paul <peiche@gmail.com>
2019-11-22 17:10:57 -0600

    Refactored taxonomy include.

--- resources/views/partials/post-tags.php
+++
@@ -1,5 +0,0 @@
-<?php Hybrid\Post\display_terms( [
-	'taxonomy' => 'post_tag',
-	'class' => 'entry__terms entry__terms--post_tag flex flex-wrap flex-gap-xxs',
-	'sep' => '',
-] );
---
+++ resources/views/partials/post-terms.php
@@ -0,0 +1,5 @@
+<?php Hybrid\Post\display_terms( [
+	'taxonomy' => $data->taxonomy,
+	'class' => 'entry__terms entry__terms--' . $data->taxonomy . ' flex flex-wrap flex-gap-xxs',
+	'sep' => '',
+] );
