eb5a47f8ab293962fcd96c5ac2eebd87409a2200
Paul <peiche@gmail.com>
2020-03-04 11:08:30 -0600

    Post navigation fix

--- resources/views/partials/post-navigation.php
+++ resources/views/partials/post-navigation.php
@@ -1,10 +1,12 @@
 <?php if ( is_single() ) : ?>
 
 	<div class="container max-width-md margin-y-lg">
-		<?php the_post_navigation( array(
-			'prev_text' => __( \Garrick\get_svg( [ 'icon' => 'arrow-left', 'garrick' => 'garrick' ] ) . 'garrick' ),
-			'next_text' => __( '%title' . \Garrick\get_svg( [ 'icon' => 'arrow-right', 'garrick' => 'garrick' ] ) ),
-		) ) ?>
+		<?php
+			the_post_navigation( array(
+				'prev_text' => \Garrick\get_svg( [ 'icon' => 'arrow-left', 'class' => 'margin-right-xxs' ] ) . __( '%title', 'garrick' ),
+				'next_text' => __( '%title', 'garrick' ) . \Garrick\get_svg( [ 'icon' => 'arrow-right', 'class' => 'margin-left-xxs' ] ),
+			) )
+		?>
 	</div>
 
 <?php endif ?>
