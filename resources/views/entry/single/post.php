aa8f9d78a3f0787ebcca74591b05c10def90ba57
Paul <peiche@gmail.com>
2020-03-16 17:20:46 -0500

    Removed duplicate template

--- resources/views/entry/single/post.php
+++
@@ -1,9 +0,0 @@
-<article <?php Hybrid\Attr\display( 'entry' ) ?>>
-
-	<?php Hybrid\View\display( 'partials', 'post-header' ) ?>
-
-	<?php Hybrid\View\display( 'partials', 'post-content' ) ?>
-
-	<?php Hybrid\View\display( 'partials', 'post-footer' ) ?>
-
-</article>
