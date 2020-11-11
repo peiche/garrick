afceff551d1d7ba6e79f3cfb38133e49a2874ea3
Paul <peiche@gmail.com>
2020-11-11 09:59:19 -0600

    Fixed margins

--- resources/views/partials/post-footer.php
+++ resources/views/partials/post-footer.php
@@ -1,30 +1,34 @@
 <footer class="entry__footer margin-top-md">
 
 	<?php if ( has_category() ) : ?>
-		<div class="flex gap-xxs margin-bottom-xxxs">
-			<div class="flex-shrink-0 margin-top-xxxs">
-				<?php echo \Garrick\get_svg( [ 'icon' => 'category', 'title' => __( 'Categories', 'garrick' ) ] ) ?>
-			</div>
-			<div>
-				<?php Hybrid\View\display( 'partials', 'post-terms', [ 'taxonomy' => 'category' ] ) ?>
+		<div class="margin-bottom-xxs">
+			<div class="flex gap-xxs">
+				<div class="flex-shrink-0 margin-top-xxxs">
+					<?php echo \Garrick\get_svg( [ 'icon' => 'category', 'title' => __( 'Categories', 'garrick' ) ] ) ?>
+				</div>
+				<div>
+					<?php Hybrid\View\display( 'partials', 'post-terms', [ 'taxonomy' => 'category' ] ) ?>
+				</div>
 			</div>
 		</div>
 	<?php endif ?>
 
 	<?php if ( has_tag() ) : ?>
-		<div class="flex gap-xxs margin-bottom-xxxs">
-			<div class="flex-shrink-0 margin-top-xxxs">
-				<?php echo \Garrick\get_svg( [ 'icon' => 'tag', 'title' => __( 'Tags', 'garrick' ) ] ) ?>
-			</div>
-			<div>
-				<?php Hybrid\View\display( 'partials', 'post-terms', [ 'taxonomy' => 'post_tag' ] ) ?>
+		<div class="margin-bottom-xxxs">
+			<div class="flex gap-xxs">
+				<div class="flex-shrink-0 margin-top-xxxs">
+					<?php echo \Garrick\get_svg( [ 'icon' => 'tag', 'title' => __( 'Tags', 'garrick' ) ] ) ?>
+				</div>
+				<div>
+					<?php Hybrid\View\display( 'partials', 'post-terms', [ 'taxonomy' => 'post_tag' ] ) ?>
+				</div>
 			</div>
 		</div>
 	<?php endif ?>
 
 	<?php edit_post_link(
 		esc_html__( 'Edit', 'garrick' ),
-		'<div class="flex gap-xxs margin-bottom-xxxs"><div class="flex-shrink-0">' . \Garrick\get_svg( [ 'icon' => 'edit' ] ) . '</div><div>',
+		'<div class="flex gap-xxs"><div class="flex-shrink-0">' . \Garrick\get_svg( [ 'icon' => 'edit' ] ) . '</div><div>',
 		'</div></div>'
 	) ?>
 
