8d139042445b7e87c3d3b8c3071a0ee331fbaa4a
Paul Eiche <peiche@gmail.com>
2021-10-09 14:53:51 -0500

    Removed fallback stylesheet.

--- resources/js/app/fallback-style.js
+++
@@ -1,6 +0,0 @@
-( function() {
-	if ( ! ( 'CSS' in window ) || ! CSS.supports( 'color', 'var(--color-var)' ) ) {
-		const css = document.getElementById( 'garrick-screen-css' );
-		css.href = css.href.replace( 'screen', 'screen-fallback' );
-	}
-}() );
